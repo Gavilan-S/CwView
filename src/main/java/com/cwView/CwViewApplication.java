27a3676d2d444d8ac45e92fbc3a2e57ad8936d6d
Gvln <santis.paez2414@outlook.com>
2024-10-27 21:10:16 -0500

    feat: inital commit add dependecies and commitlint

---
+++ src/main/java/com/cwView/CwViewApplication.java
@@ -0,0 +1,13 @@
+package com.cwView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwView/CwViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
