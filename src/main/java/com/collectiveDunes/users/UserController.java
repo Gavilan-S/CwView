60b21c451724a87b6d096c2129f39e0c06cdc57e
Oscarvm117 <oscarvemo@unisabana.edu.co>
2024-11-17 12:44:05 -0500

    logins

--- src/main/java/com/collectiveDunes/users/UserController.java
+++ src/main/java/com/collectiveDunes/users/UserController.java
@@ -1,8 +1,16 @@
 package com.collectiveDunes.users;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -12,7 +20,7 @@ public class UserController {
     private UsersRepository userRepository;
 
     @PostMapping("/insert")
-    public String addUser(String username, @RequestParam String password) {
+    public String addUser(@RequestParam String username, @RequestParam String password) {
         if (userRepository.findByUsername(username) != null) {
             return "Error: Username already exists";
         }
@@ -20,8 +28,31 @@ public class UserController {
         user.setUsername(username);
         user.setPassword(password); 
         userRepository.save(user);
-        return "User added successfully";
+        return "redirect:/portal";
     }
+
+    @PostMapping("/login")
+public ResponseEntity<Map<String, String>> loginUser(@RequestParam String username, @RequestParam String password) {
+    User user = userRepository.findByUsername(username);
+
+    Map<String, String> response = new HashMap<>();
+    if (user == null) {
+        response.put("status", "error");
+        response.put("message", "Username does not exist");
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    if (!user.getPassword().equals(password)) {  // Verificación de contraseña
+        response.put("status", "error");
+        response.put("message", "Invalid password");
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    response.put("status", "success");
+    response.put("message", "Login successful");
+    return ResponseEntity.ok(response);
+}
+
 
     @GetMapping("/findAll")
     public Iterable<User> getAllUsers() { return userRepository.findAll(); }
